5ca7645e158717af566dfcfabf88843078ab181f
Leovaldez42 <gaurav4.037@gmail.com>
2020-06-03 14:09:32 +0530

    Refactoring donegit add .!

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDT5a0t_Ib6P2WU_O4VJEsb8WxNEyoWZ_I",
+    authDomain: "socialape-6849e.firebaseapp.com",
+    databaseURL: "https://socialape-6849e.firebaseio.com",
+    projectId: "socialape-6849e",
+    storageBucket: "socialape-6849e.appspot.com",
+    messagingSenderId: "221771358210",
+    appId: "1:221771358210:web:b60c6448d596545dc4c71b",
+    measurementId: "G-FYZ4LPS9DH"
+};
